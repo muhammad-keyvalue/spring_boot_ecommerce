9ffd3c7b5dce012b2f6ba34a1202a3d2ea5c903a
Muhammad <muhammad.cv@keyvalue.systems>
2023-04-18 12:06:17 +0530

    fix: Rename entity from order to orders

---
+++ src/main/java/com/sample/ecommerce/order/model/Orders.java
@@ -0,0 +1,64 @@
+package com.sample.ecommerce.order.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.GenerationType;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.sample.ecommerce.order.constants.OrderStatus;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Orders {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name="status")
+  private OrderStatus status;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
+  @UpdateTimestamp
+  private LocalDateTime updatedAt;
+
+  @Column(nullable = false)
+  private int totalQuantity;
+
+  @Column(nullable = false)
+  private int totalPrice;
+
+  @ManyToOne
+  @JoinColumn(name = "customer_id")
+  @JsonIgnore
+  private Customer customer;
+
+  @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+  private List<OrderItem> orderItems;
+
+}
