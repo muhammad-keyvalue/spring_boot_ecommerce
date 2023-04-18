f3071f263b5ea9ab351d099b9aa997e04ce50dfc
Muhammad <muhammad.cv@keyvalue.systems>
2023-04-18 12:15:43 +0530

    Revert "feat: Define repositories for customer and product"
    This reverts commit e77aaf9f3df3232c1307aff6d4bd6f8539a5865b.

--- src/main/java/com/sample/ecommerce/order/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sample.ecommerce.order.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sample.ecommerce.order.model.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer,Integer> {
-  
-}
