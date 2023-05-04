2a75e6fc98b4d6c5287f8315de4a35350ef13a76
Muhammad <muhammad.cv@keyvalue.systems>
2023-05-04 16:18:09 +0530

    Fix: Rename foreign key constraints in migration

--- src/main/resources/db/migration/V1.1__create_tables.sql
+++ src/main/resources/db/migration/V1.1__create_tables.sql
@@ -3,7 +3,7 @@ create table customer (id serial not null, city varchar(255), country_code varch
 create table "order" (id serial not null, created_at timestamp(6), status varchar(255), total_price integer not null, total_quantity integer not null, updated_at timestamp(6), customer_id integer, primary key (id));
 create table order_item (id serial not null, quantity integer, order_id integer, product_id integer, primary key (id));
 create table product (id serial not null, description varchar(255), image varchar(255), name varchar(255), unit_price integer, category_id integer, primary key (id));
-alter table if exists "order" add constraint FK1oduxyuuo3n2g98l3j7754vym foreign key (customer_id) references customer;
-alter table if exists order_item add constraint FKs234mi6jususbx4b37k44cipy foreign key (order_id) references "order";
-alter table if exists order_item add constraint FK551losx9j75ss5d6bfsqvijna foreign key (product_id) references product;
-alter table if exists product add constraint FK1mtsbur82frn64de7balymq9s foreign key (category_id) references category;
+alter table "order" add constraint "FK_Order_Customer" foreign key (customer_id) references customer;
+alter table order_item add constraint "FK_OrderItem_Order" foreign key (order_id) references "order";
+alter table order_item add constraint "FK_OrderItem_Product" foreign key (product_id) references product;
+alter table product add constraint "FK_Product_Category" foreign key (category_id) references category;
