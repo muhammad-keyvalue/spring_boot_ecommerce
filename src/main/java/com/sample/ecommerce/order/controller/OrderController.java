19e284103512a52236d8bcfcff356964e3652628
Muhammad <muhammad.cv@keyvalue.systems>
2023-05-08 15:10:03 +0530

    Feat: Change response-status of create and update to 201

--- src/main/java/com/sample/ecommerce/order/controller/OrderController.java
+++ src/main/java/com/sample/ecommerce/order/controller/OrderController.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -32,6 +34,7 @@ public class OrderController {
    */
 
   @PostMapping()
+  @ResponseStatus(HttpStatus.CREATED)
   public Order create(@Valid @RequestBody OrderDto createOrder) {
     return orderservice.create(createOrder);
   }
@@ -56,6 +59,7 @@ public class OrderController {
 
 
   @PutMapping("/{id}")
+  @ResponseStatus(HttpStatus.CREATED)
   public Order update(@PathVariable int id, @Valid @RequestBody UpdateOrderDto updateOrder) {
     return orderservice.update(id, updateOrder);
   }
